b3f829d24f7369974a377ffc07a6da91748a8503
Nate-yu <2242936124@qq.com>
2022-12-30 21:49:35 +0800

    在转换温度中加入循环

--- Code/TempConvert.py
+++ Code/TempConvert.py
@@ -1,9 +1,11 @@
 TempStr = input("请输入带有符号的温度值：")
-if TempStr[-1] in ['F','f']:
-    C = (eval(TempStr[0:-1]) - 32) / 1.8
-    print("转换后的温度是{:.2f}C".format(C))
-elif TempStr[-1] in ['C','c']:
-    F = 1.8 * eval(TempStr[0:-1]) + 32
-    print("转换后的温度是{:.2f}F".format(F))
-else:
-    print("输入格式错误")
+while TempStr[-1] not in ['N','n']: # 循环直到用户输入的最后一个字符是'N'或'n'时退出
+    if TempStr[-1] in ['F','f']: # 判断字符串TempStr的最后一个字符是否为'F'或'f'
+        C = (eval(TempStr[0:-1]) - 32) / 1.8
+        print("转换后的温度是{:.2f}C".format(C))
+    elif TempStr[-1] in ['C','c']:
+        F = 1.8 * eval(TempStr[0:-1]) + 32
+        print("转换后的温度是{:.2f}F".format(F))
+    else:
+        print("输入格式错误")
+    TempStr = input("请输入带有符号的温度值：")
