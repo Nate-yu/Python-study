2d571cf2b7e32c5cee425dd509ad0492c0564025
Nate-yu <2242936124@qq.com>
2022-12-31 20:49:41 +0800

    数字类型的使用及相关笔记

---
+++ Code/DataType.py
@@ -0,0 +1,19 @@
+""" print(pow(2,100))
+print(pow(2,pow(2,15))) # 嵌套使用 """
+
+""" print(100 / 3) # 浮点数
+print(100 // 3) # 整数
+print(123 + 4.0) # 浮点数
+print(10.0 - 1 + 2j) # 复数 """
+
+""" print(abs(-2))
+print(divmod(8,3))
+print(pow(2,3,3))
+print(round(2.3353,2))
+print(max(1,2,3,4))
+print(min(1,2,3,4)) """
+
+print(int(10.99))
+print(complex(10.99))
+# print(float(10+99j)) # 解释器会报TypeError错误
+print(float((10+99j).imag))
