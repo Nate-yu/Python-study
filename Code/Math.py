b55ae8840fb59ad1abfd3c424eefa0c39b4240d4
Nate-yu <2242936124@qq.com>
2023-01-02 15:02:16 +0800

    math库的使用

---
+++ Code/Math.py
@@ -0,0 +1,10 @@
+import math
+print(math.fabs(-2))
+print(math.fsum([0.1,0.2,0.3]),0.1+0.2+0.3)
+print(math.factorial(3))
+print(math.gcd(60,24))
+
+print(math.pow(2,3))
+print(math.exp(2))
+print(math.sqrt(math.exp(2)))
+print(math.log(2,4),math.log(math.exp(1)))
