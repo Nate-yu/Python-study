4608d6f94af9eb0d826872f6e617ef3ab9e6c200
Nate-yu <2242936124@qq.com>
2023-01-03 22:46:03 +0800

    单行动态刷新

--- Code/TextProgressBar.py
+++ Code/TextProgressBar.py
@@ -1,10 +1,14 @@
 # 文本进度条
 import time
-scale = 10
+""" scale = 10
 print("-----执行开始-----")
 for i in range(scale + 1):
     a, b = '**' * i, '..' * (scale-i)
     c = (i / scale) * 100
     print("%{:^3.0f}[{}->{}]".format(c,a,b))
     time.sleep(0.1)
-print("-----执行结束-----")
+print("-----执行结束-----") """
+
+for i in range(101):
+    print("\r{:3}%".format(i),end = "")
+    time.sleep(0.05)
