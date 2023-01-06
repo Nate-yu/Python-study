a22e0a1f6d2f41dfdc89eb4ad838c67bb8d45c76
Nate-yu <2242936124@qq.com>
2023-01-06 10:40:09 +0800

    循环保留字：break和continue

---
+++ Code/Unit4/BreakContinue.py
@@ -0,0 +1,10 @@
+""" for s in "BIT":
+    for i in range(10):
+        print(s,end = "")
+        if s == "I":
+            break """
+
+for s in "PYTHON":
+    if s == "T":
+        continue
+    print(s,end = "")
