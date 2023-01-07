7147e0b849ad5824890715b36f78eb7e707e53b8
Nate-yu <2242936124@qq.com>
2023-01-07 14:46:20 +0800

    优化猜数游戏与因此而作出的总结

--- Code/exercises/GuessNum.py
+++ Code/exercises/GuessNum.py
@@ -1,13 +1,22 @@
-# 在程序中预设一个0~9之间的整数，让用户通过键盘输入所猜的数
+# 在程序中预设一个0~100之间的整数，让用户通过键盘输入所猜的数
 import random
 count = 1
-num = random.randint(0,9)
-Num = eval(input("请输入一个0~9之间的数："))
-while num != Num:
-    if Num > num:
-        print("遗憾，太大了！")
-    if Num < num:
-        print("遗憾，太小了！")
-    Num = eval(input("请输入一个0~9之间的数："))
-    count += 1
-print("预测{}次，你猜中了！".format(count))
+num = random.randint(0,100)
+while True:
+    try:
+        Num = eval(input("请输入一个0~100之间的数："))
+        begin, end = 0, 100
+        while num != Num:
+            if Num > num:
+                print("遗憾，太大了！")
+                end = Num
+                Num = eval(input("请输入一个{}~{}之间的数：".format(begin, end)))
+            if Num < num:
+                print("遗憾，太小了！")
+                begin = Num
+                Num = eval(input("请输入一个{}~{}之间的数：".format(begin, end)))
+            count += 1
+        print("预测{}次，你猜中了！".format(count))
+        break
+    except:
+        print("输入内容必须为整数！")
