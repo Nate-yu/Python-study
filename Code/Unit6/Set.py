db457db7c0eacdff5ae2aa0f4a548bfcd47701c7
Nate-yu <2242936124@qq.com>
2023-01-12 16:02:20 +0800

    组合数据类型概述

---
+++ Code/Unit6/Set.py
@@ -0,0 +1,16 @@
+S = {425, "BIT", (10, "CS"), 424}
+print(S)
+T = {425, "BIT", (10, "CS"), 424, 425}
+print(T)
+
+W = set("apple")
+print(W)
+V = set(("cat","dog","tiger","human"))
+print(V)
+
+S.add("apple")
+print(S)
+print(S.pop())
+S.discard("apple")
+print(S)
+print(len(S))
